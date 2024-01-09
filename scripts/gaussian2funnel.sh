511e682cd088055851acdfbb127b1fd302b78931
Ezra Erives <erives@mit.edu>
2024-01-09 16:15:08 -0500

    Create testing scripts in dedicated directory

---
+++ scripts/gaussian2funnel.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+#
+# Generate a random number
+random_number=$RANDOM
+
+# Call the Python script with all parameters inline
+python train.py \
+--run_name=gaussian2funnel_$random_number \
+--source=gaussian \
+--target=funnel \
+--data_dim=10 \
+--n_epochs=50 \
+--loss=reverse_kl_loss \
+--lr=0.001 \
+--scheduler=plateau \
