a361de067b806b63a00dd90b63a9235c547d99dd
Ezra Erives <erives@mit.edu>
2024-01-20 15:23:16 -0500

    Implement wandb logging

--- train_phase_space_cnf.py
+++ train_phase_space_cnf.py
@@ -2,11 +2,13 @@ import os
 import sys
 
 import pytorch_lightning as pl
+from lightning.pytorch.loggers import WandbLogger
 
 sys.path.append('../')
 from model.cnf import PhaseSpaceCNF
 from utils.parsing import parse_phase_space_cnf_args
 
+PROJECT_NAME = 'verlet-flows'
 
 def main():
     args = parse_phase_space_cnf_args()
@@ -14,8 +16,15 @@ def main():
     # Initialize flow
     cnf = PhaseSpaceCNF(args)
 
+    # Initialize logger
+    wandb_logger = WandbLogger(
+        project=PROJECT_NAME,
+        name=args.run_name,
+        save_dir='workdir',
+    )
+
     # Train
-    trainer = pl.Trainer(min_epochs=args.num_epochs, max_epochs=args.num_epochs)
+    trainer = pl.Trainer(logger=wandb_logger, min_epochs=args.num_epochs, max_epochs=args.num_epochs)
     trainer.fit(cnf)
 
     # Save
